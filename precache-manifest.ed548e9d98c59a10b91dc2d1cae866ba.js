92663f2bf0e39f5d4b74accb414c26b7492d6687
dan <pigpig850130@gmail.com>
2021-10-17 20:31:52 +0800

    Updates

---
+++ precache-manifest.ed548e9d98c59a10b91dc2d1cae866ba.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5efb29c33d31f39193fecc4a9686d89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83246e60e7b494a00688",
+    "url": "/static/css/main.d176facd.chunk.css"
+  },
+  {
+    "revision": "cbf0f76f449e226caf66",
+    "url": "/static/js/2.8dfe5492.chunk.js"
+  },
+  {
+    "revision": "0732114d20492cec2812943924b2724e",
+    "url": "/static/js/2.8dfe5492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83246e60e7b494a00688",
+    "url": "/static/js/main.58a21b68.chunk.js"
+  },
+  {
+    "revision": "37b9d1bea5f960996d46",
+    "url": "/static/js/runtime-main.e107559d.js"
+  },
+  {
+    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
+    "url": "/static/media/OpenSans-Bold.f5331cb6.ttf"
+  },
+  {
+    "revision": "49f89e34d03233b1f27788f75df7a40a",
+    "url": "/static/media/OpenSans-ExtraBold.49f89e34.ttf"
+  },
+  {
+    "revision": "9ff12f694e5951a6f51a9d63b05062e7",
+    "url": "/static/media/OpenSans-Light.9ff12f69.ttf"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/static/media/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "e1c83f9474e0cc1d84a13c6d1ddf3ca5",
+    "url": "/static/media/OpenSans-SemiBold.e1c83f94.ttf"
+  },
+  {
+    "revision": "471b312718c6576029e10a833ef17c6d",
+    "url": "/static/media/clipboard.471b3127.svg"
+  },
+  {
+    "revision": "9fd953dd47bc3cae20e845f187c3097c",
+    "url": "/static/media/github-green.9fd953dd.svg"
+  },
+  {
+    "revision": "121398c08750c40d288ee939616ac0d0",
+    "url": "/static/media/github.121398c0.svg"
+  }
+]);
